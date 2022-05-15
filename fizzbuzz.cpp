1d06f23305c98527ff03a543593dbb7802d3d51d
Hiter34 <68299210+Hiter34@users.noreply.github.com>
2022-05-15 11:01:06 +0300

    FizzBuzz task

---
+++ fizzbuzz.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void FizzBuzz(int number = 1)
+{
+	if (number < 101)
+	{
+		if (number % 3 == 0) std::cout << "Fizz";
+		if (number % 5 == 0) std::cout << "Buzz";
+		if (number % 3 != 0 && number % 5 != 0) std::cout << number;
+		std::cout << std::endl;
+		FizzBuzz(number + 1);
+	}
+	
+}
+
+
+int main()
+{
+	FizzBuzz();
+}
+
